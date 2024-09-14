7ed6e44d9e1a9baa2046f78ef48becb107e343be
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-14 20:46:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let input1 = prompt("Enter the first number:");
-let input2 = prompt("Enter the second number:");
+let input1 = Number(prompt("Enter the first number:"));
+let input2 = Number(prompt("Enter the second number:"));
+let input3=input1+input2;
+if(typeof input1!="number" || typeof input3!="number" ||input1==""||input2==""){
+	alert("Invalid input. Please enter a valid number")
+}
+else{
+	alert("The sum of "+input1+" "+"and "+input2+" is "+input3)
+}
 
 // Write your code here and print the output using alert function
